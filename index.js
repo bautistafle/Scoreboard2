85fd3a9fbd40f07818b12de87ffd46ccf1d29036
bautistafle <bautistafle@gmail.com>
2023-02-17 18:21:11 -0600

    Scoreboard2

---
+++ index.js
@@ -0,0 +1,17 @@
+let agregaUnoBoton = document.getElementById("addUnoBoton")
+let agregaDosBoton = document.getElementById("addDosBoton")
+let agregaUnoMas = document.getElementById("casa")
+let incrementByOne = 0
+let incrementByTwo = 0
+
+
+function addAgregaHome(){
+    
+   incrementByOne += 1
+    agregaUnoMas.textContent = incrementByOne
+
+}
+function addAgregaHome2(){    
+     incrementByTwo += 2
+    agregaUnoMas.textContent = incrementByTwo
+}
